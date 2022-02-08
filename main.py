25be0d965535aaf4f5ecf7afdfdd1745bf45f3f8
Rangga Prangwedana <vergil1st@gmail.com>
2022-02-08 21:21:35 +0700

    Basic Movements

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 import pygame
-import time
+from pygame.locals import *
 
-if __name__ == "__main__":
+def main():
+    def draw_block():
+        surface.fill((110, 110, 5))
+        surface.blit(block, (block_x, block_y))
+        pygame.display.flip()    
+    
     pygame.init()
 
     surface = pygame.display.set_mode((500,500))
-    time.sleep(5)
+    surface.fill((110, 110, 5))
+    pygame.display.flip()
+
+    block = pygame.image.load("resources/block.jpg").convert()
+    block_x = 100
+    block_y = 100 
+    surface.blit(block,(block_x, block_y))
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+                #Movement keys
+                if event.key == K_UP:
+                    block_y -= 10
+                    draw_block()
+                if event.key == K_DOWN:
+                    block_y += 10
+                    draw_block()
+                if event.key == K_LEFT:
+                    block_x -= 10
+                    draw_block()
+                if event.key == K_RIGHT:
+                    block_x += 10
+                    draw_block()                
+            elif event.type == QUIT:
+                running = False    
+
+if __name__ == "__main__":
+    main()    
